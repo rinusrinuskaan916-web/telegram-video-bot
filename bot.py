c9a565fa68314b6ebc1177cf45e5ccbdc441638b
rinusrinuskaan916-web <rinusrinuskaan916@gmail.com>
2026-03-12 18:13:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,22 +8,30 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
     url = update.message.text
 
+    await update.message.reply_text("Downloading video... ⏳")
+
     ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',
+        'merge_output_format': 'mp4',
         'outtmpl': 'video.%(ext)s'
     }
 
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
 
-    for file in os.listdir():
-        if file.startswith("video"):
-            await update.message.reply_video(video=open(file, "rb"))
-            os.remove(file)
-            break
+        for file in os.listdir():
+            if file.startswith("video"):
+                await update.message.reply_video(video=open(file, "rb"))
+                os.remove(file)
+                break
+
+    except Exception as e:
+        await update.message.reply_text("Download failed ❌")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
-app.add_handler(MessageHandler(filters.TEXT, download))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, download))
 
 print("Bot running...")
 app.run_polling()
